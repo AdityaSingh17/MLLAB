5ac6bcb0009be4d5c12de05664b8eb8dd8ab061c
AdityaSingh17 <adityarsingh17@gmail.com>
2020-11-14 09:42:08 +0530

    Update Candidate Elimination Program

--- Program 2/Candidate.py
+++ Program 2/Candidate.py
@@ -1,3 +1,5 @@
+# For a given set of training data examples stored in a .CSV file, implement and demonstrate the Candidate-Elimination algorithmto output a description of the set of all hypotheses consistent with the training examples.
+
 import csv
 
 a = []
